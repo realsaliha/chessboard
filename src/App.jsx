49581b1383c25f589e90bf36e94449fc69b8c615
realsaliha <salihajaved001@gmail.com>
2026-03-31 19:24:49 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Chess from "./components/Chess";
+import "./style.css";
+export default function App() {
+  return (
+    <>
+    <Chess />
+    </>
+  );
+}
---
+++ src/components/Chess.jsx
@@ -0,0 +1,84 @@
+export default function Chess() {
+    return (
+       <>
+       <div class="chessboard">
+        
+         <div class="orange">&#9820;</div>
+         <div class="skin">&#9816;</div>
+         <div class="orange">&#9821;</div>
+         <div class="skin">&#9813;</div>
+         <div class="orange">&#9818;</div>
+         <div class="skin">&#9821;</div>
+         <div class="orange">&#9822;</div>
+         <div class="skin">&#9820;</div>
+          
+         <div class="skin">&#9823;</div>
+         <div class="orange">&#9823;</div>
+         <div class="skin">&#9823;</div>
+         <div class="orange">&#9823;</div>
+         <div class="skin">&#9823;</div>
+         <div class="orange">&#9823;</div>
+         <div class="skin">&#9823;</div>
+         <div class="orange">&#9823;</div>
+         
+         <div class="orange"></div>
+         <div class="skin"></div>
+         <div class="orange"></div>
+         <div class="skin"></div>
+         <div class="orange"></div>
+         <div class="skin"></div>
+         <div class="orange"></div>
+         <div class="skin"></div>
+          
+         <div class="skin"></div>
+         <div class="orange"></div>
+         <div class="skin"></div>
+         <div class="orange"></div>
+         <div class="skin"></div>
+         <div class="orange"></div>
+         <div class="skin"></div>
+         <div class="orange"></div>
+           
+         <div class="orange"></div>
+         <div class="skin"></div>
+         <div class="orange"></div>
+         <div class="skin"></div>
+         <div class="orange"></div>
+         <div class="skin"></div>
+         <div class="orange"></div>
+         <div class="skin"></div>
+          
+         <div class="skin"></div>
+         <div class="orange"></div>
+         <div class="skin"></div>
+         <div class="orange"></div>
+         <div class="skin"></div>
+         <div class="orange"></div>
+         <div class="skin"></div>
+         <div class="orange"></div>
+           
+         <div class="orange">&#9817;</div>
+         <div class="skin">&#9817;</div>
+         <div class="orange">&#9817;</div>
+         <div class="skin">&#9817;</div>
+         <div class="orange">&#9817;</div>
+         <div class="skin">&#9817;</div>
+         <div class="orange">&#9817;</div>
+         <div class="skin">&#9817;</div>
+          
+         <div class="skin">&#9814;</div>
+         <div class="orange">&#9816;</div>
+         <div class="skin">&#9815;</div>
+         <div class="orange">&#9813;</div>
+         <div class="skin">&#9812;</div>
+         <div class="orange">&#9815;</div>
+         <div class="skin">&#9813;</div>
+         <div class="orange">&#9814;</div>
+
+
+          
+          
+    </div> 
+       </>
+    );
+}      
